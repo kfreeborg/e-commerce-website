c44f1ecadb8cff287d9d7c604da6ac7749490fee
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-01-07 23:25:01 -0700

    get categories working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,34 +1,34 @@
-const router = require("express").Router();
-const { Category, Product } = require("../../models");
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
+  // find all categories
   const category = await Category.findAll({
+    // be sure to include its associated Products
     include: {
       model: Product,
-      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
     },
   });
   res.json(category);
-  // find all categories
-  // be sure to include its associated Products
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
   // create a new category
 });
 
-router.put("/:id", (req, res) => {
+router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
 });
 
--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
