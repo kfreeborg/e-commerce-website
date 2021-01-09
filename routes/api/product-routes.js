5e9d080ba55ad71375e0ea2de943fb6be256f4cb
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2021-01-09 15:51:04 -0700

    prodcut delete working

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,8 +118,14 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  const product = await Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  });
+  res.json(product);
 });
 
 module.exports = router;
